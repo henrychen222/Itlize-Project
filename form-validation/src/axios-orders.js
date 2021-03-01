024e7f75079ef736bec90d1bef4eaa230dbcdc14
henrychen222 <henrychen940822@gmail.com>
2021-03-01 02:39:45 -0500

    022821 night

--- form-validation/src/axios-orders.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create({
-    // baseURL: 'https://react-my-burger.firebaseio.com/'
-    baseURL: 'https://form-validation-react.firebaseio.com/'
-});
-
-export default instance;
